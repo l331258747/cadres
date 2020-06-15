f939ff2bfa23b38ad5702e946f5a37f4be3ce1f1
liguoqiang <331258747@qq.com>
2020-06-15 23:45:32 +0800

    base

---
+++ Cadres/app/src/main/java/com/example/cadres/view/home/HomeActivity.java
@@ -0,0 +1,20 @@
+package com.example.cadres.view.home;
+
+import com.example.cadres.base.BaseActivity;
+
+public class HomeActivity extends BaseActivity {
+    @Override
+    public int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+}
